a131d8e4d3db0a087258567bfadae6a19c8d87e7
backdoorkain <51509658+backdoorkain@users.noreply.github.com>
2026-07-08 02:18:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Rcon } = require('rcon-client');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 // Variables de entorno para RCON
 const RCON_HOST = process.env.RCON_HOST || '62.171.153.80';
